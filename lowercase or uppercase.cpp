e5006f5d16f17a832f58cf2076c7db58b915c0f9
ZAIN IQBAL <124054465+Zaini04@users.noreply.github.com>
2023-02-08 21:26:17 +0500

    Add files via upload

---
+++ lowercase or uppercase.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <ctype.h>
+
+int main() {
+    char letter;
+    cout << "Enter a letter: ";
+    cin >> letter;
+    int result = islower(letter);
+    cout << "Result: " << result << endl;
+    return 0;
+}
